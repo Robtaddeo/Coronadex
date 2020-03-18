9ce3f38db560818b506d2363b268ab03b419288b
Robert Taddeo <robtaddeo@gmail.com>
2020-03-18 17:05:22 -0400

    Adding react frontend components

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Header } from './Header';
+import { makeStyles, StylesProvider } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+    dashboard: {
+    },
+    root: {
+        flexGrow: 1,
+        width: '95%',
+        margin: '0 auto',
+      },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        backgroundColor: '#57606f',
+        color: 'white',
+        minHeight: '500px'
+    },
+    
+  }));
 
 export const Dashboard = () =>  {
-
+    const classes = useStyles();
 
     return(
-        <div>
+        <div className={classes.dashboard}>
             <Header />
+            <div className={classes.root}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                    <Paper className={classes.paper}>Graph will go here</Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <Paper className={classes.paper}>Another Graph will go here</Paper>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,39 +2,160 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
+    navbar: {
+        backgroundColor: '#2c3e50',
+        marginBottom: "20px"
+    },
+    grow: {
+    flexGrow: 1,
+    },
+    formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    color: 'white'
+    },
+    selectEmpty: {
+    marginTop: theme.spacing(2),
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    justifyContent: 'flex-end',
+    float: "right"
     },
-    title: {
-      flexGrow: 1,
+    exchange: {
+    fontSize: '15px',
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+        display: 'block',
     },
-  }));
+    },
+    inputRoot: {
+    color: 'inherit',
+    },
+    inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+        width: '20ch',
+    },
+    },
+    sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+        display: 'flex',
+    },
+    },
+    sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+        display: 'none',
+    },
+    },
+}));
   
-  export const Header = () => {
-    const classes = useStyles();
+
+export const Header = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const indexes = [
+      {
+          "key" : "SPX",
+          "name" : "S&P 500",
+          "value" : "SPX",
+      },
+      {
+        "key" : "DJIA",
+        "name" : "Dow Jones",
+        "value" : "DJIA",
+    }
+  ]
   
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography className={classes.exchange} variant="h6" noWrap>
+            Exchange
+          </Typography>
+          <FormControl className={classes.formControl}>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={"SPX"}
+                onChange={handleChange}
+                >
+                {indexes.map(index => {
+                    return(<MenuItem key={index.key} value={index.value}>{index.name}</MenuItem>)
+                })}
+            </Select>
+            </FormControl>
+            
+            
+            <FormControl className={classes.formControl}>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                onChange={handleChange}
+                >
+            </Select>
+            </FormControl>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </div>
+  );
+}
